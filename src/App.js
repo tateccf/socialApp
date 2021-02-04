0be70e2daf76ceb6d4f303b2b3d70120e47275f7
frandev <franbajo@gmail.com>
2021-02-04 10:05:30 +0100

    Terms and About page has been created. React Router Dom has been installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import { BrosweRouter as Router, Switch, Route } from 'react-router-dom';
 
 // ==== MY COMPONENTS ====
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 // ==== MY Pages ====
-
 import HomeGuest from './pages/HomeGuest';
 
 const App = () => {
