cd6830c4c56af721f4825a3674d1f8e6490c90da
frandev <franbajo@gmail.com>
2021-02-04 12:17:31 +0100

    Setting up Firebase basic configuration

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import env from 'react-dotenv';
+
+const firebaseConfig = {
+  apiKey: env.API_KEY,
+  authDomain: env.AUTH_DOMAIN,
+  projectId: env.PROJECT_ID,
+  storageBucket: env.STORAGE_BUCKET,
+  messagingSenderId: env.MESSAGING_SENDER,
+  appId: env.APP_ID,
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const db = firebaseApp.firestore();
