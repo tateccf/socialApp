87db57b548d793bbe69a27853c7b468f3ce32a20
frandev <franbajo@gmail.com>
2021-02-09 09:15:41 +0100

    Show Edit/Delete post button ONLY if the user is the owner of the post

--- src/pages/ViewSinglePost.js
+++ src/pages/ViewSinglePost.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import StateContext from '../context/StateContext';
 import { db } from '../firebase';
 import gravatarUrl from 'gravatar-url';
 import { Link } from 'react-router-dom';
@@ -12,7 +13,7 @@ import NotFound from '../pages/NotFound';
 const ViewSinglePost = () => {
   // Get the post ID from the page URL
   const { id } = useParams();
-
+  const appState = useContext(StateContext);
   const [post, setPost] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [notFound, setNotFound] = useState(false);
@@ -37,32 +38,43 @@ const ViewSinglePost = () => {
   if (notFound) return <NotFound />;
   if (isLoading) return <Spinner />;
 
+  function isOwner() {
+    if (appState.loggedIn) {
+      return appState.user.userId === post.createdBy;
+    }
+    return false;
+  }
+
   return (
     <Page title="Single Post">
       <div className="d-flex justify-content-between">
         <h2>{post.title}</h2>
-        <span className="pt-2">
-          <Link
-            to={`/post/${id}/edit/`}
-            data-tip="Edit Post"
-            data-for="edit"
-            className="text-primary mr-2"
-            title="Edit"
-          >
-            <i className="fas fa-edit"></i>
-          </Link>
-          <ReactTooltip id="edit" />
-          <Link
-            to="#"
-            data-tip="Delete Post"
-            data-for="delete"
-            className="delete-post-button text-danger"
-            title="Delete"
-          >
-            <i className="fas fa-trash"></i>
-          </Link>
-          <ReactTooltip id="delete" />
-        </span>
+
+        {/* SHOW EDIT AND DELETE BUTTON ONLY IF THE USER IS THE OWNER OF THE POST  */}
+        {isOwner() && (
+          <span className="pt-2">
+            <Link
+              to={`/post/${id}/edit/`}
+              data-tip="Edit Post"
+              data-for="edit"
+              className="text-primary mr-2"
+              title="Edit"
+            >
+              <i className="fas fa-edit"></i>
+            </Link>
+            <ReactTooltip id="edit" />
+            <Link
+              to="#"
+              data-tip="Delete Post"
+              data-for="delete"
+              className="delete-post-button text-danger"
+              title="Delete"
+            >
+              <i className="fas fa-trash"></i>
+            </Link>
+            <ReactTooltip id="delete" />
+          </span>
+        )}
       </div>
 
       <p className="text-muted small mb-4">
